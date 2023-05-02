131419a00c5e058cd14774d85fde72e566c2c497
GMX\002NHH744 <gouravbahare9494@gmail.com>
2023-05-02 08:54:37 +0530

    DB connection

--- server/app.js
+++ server/app.js
@@ -1,18 +1,33 @@
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
 
-// middleWare
+dotenv.config({path: './config.env'});
 
+const DB = process.env.DB
+
+mongoose.connect(DB, {
+    useNewUrlParser: true,
+    // useCreateIndex: true,
+    useUnifiedTopology: true,
+    // useFindAndModify: false
+}).then(() => {
+    console.log(`DB connected`);
+}).catch((err) => { console.log(`not connected`); });
+
+// middleWare
 const middleware = (req, res, next) => {
+    console.log("hello middleware")
     next();
 }
-
 
 app.get('/', (req, res) => {
     res.send(`hello server`);
 })
 
 app.get('/about', middleware, (req, res) => {
+    console.log("hello about page")
     res.send(`hello about`);
 })
 
